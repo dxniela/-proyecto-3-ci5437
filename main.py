81938a191d6bdf2e3bf2f1c6e95c7a90f47cc0d8
PanquecaFuriosa <18-10761@usb.ve>
2024-03-21 13:08:22 -0400

    adding a client function

--- main.py
+++ main.py
@@ -1,18 +1,23 @@
 from translator import translator_JSON
 import json
 
-# Abre el archivo json
-with open('./test0.json', 'r') as f:
-    data = json.load(f)
+def open_and_translate_file(filename):
+    # Abre el archivo json
+    with open(filename, 'r') as f:
+        data = json.load(f)
+    return translator_JSON(data)
 
-dates, hours, participants = translator_JSON(data)
+def main():
+    filename = input("Ingrese el nombre del archivo JSON: ")
+    dates, hours, participants = open_and_translate_file(filename)
+    print("Diccionario de fechas")
+    print(dates)
+    print("-----------------------------------------------")
+    print("Diccionario de horas")
+    print(hours)
+    print("-----------------------------------------------")
+    print("Diccionario de participantes")
+    print(participants)
+    print("-----------------------------------------------")
 
-print("Diccionario de fechas")
-print(dates)
-print("-----------------------------------------------")
-print("Diccionario de horas")
-print(hours)
-print("-----------------------------------------------")
-print("Diccionario de participantes")
-print(participants)
-print("-----------------------------------------------")
+main()
